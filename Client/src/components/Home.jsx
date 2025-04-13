7e285dcdb352f33e5b76d5bc774ddcbd2d9c1abb
Shubham Rathore <145218912+Rathore-shubham@users.noreply.github.com>
2025-04-13 11:42:10 +0530

    Update Home.jsx
    adjusted bottom spacing for mobile

--- Client/src/components/Home.jsx
+++ Client/src/components/Home.jsx
@@ -66,7 +66,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="absolute xs:bottom-10 bottom-[1rem] w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-[-3rem] w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[30px] h-[50px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
